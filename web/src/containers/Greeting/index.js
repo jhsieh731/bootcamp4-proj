c1819f86499b37c6dcc4bc7004277fec5250a12b
cathystanton <cjstanton@college.harvard.edu>
2022-01-18 12:06:14 -0500

    beginning of a greeting page

--- web/src/containers/Greeting/index.js
+++ web/src/containers/Greeting/index.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CheckToken from "../CheckToken"
-import { InfoBox } from "./styles"
+import { InfoBox, WelcomeText, CenteredText } from "./styles"
+import { DateTime } from 'luxon'
+
 
 const Greeting = () => {
   const user_id = localStorage.getItem('user_id')
+  const [date, setDate] = useState(Date().toLocaleString().split(' '))
+  const Update = (props) => {
+      setInterval(props.onInterval, 1000)
+      const text = props.content
+      const formatText = text.split('\n').map(str => <p>{ str }</p>)
+      return <CenteredText>{ formatText }</CenteredText>
+  }
+  const convert = (time) => {
+      let toD;
+      if (Number(time.split(':')[0]) > 11) {
+          toD = 'PM'
+      } else {
+          toD = 'AM'
+      }
+      return ` ${DateTime.fromFormat(time, 'HH:mm:ss').toFormat('h:mm:ss')} ${ toD }.`
+  }
   
   return (
     <CheckToken>
       <div>
         <InfoBox>
-          Hello, World
+          <WelcomeText>Get Ready to Procrastinate with Purpose</WelcomeText>
+          <Update onInterval={ () => setDate(Date().toLocaleString().split(' ')) } content=
+                { `Today is ${ date[0] }, ${ date[1] } ${ date[2] }, ${ date[3] }.\n
+                Local Time: ${ convert(date[4]) }\n` } />
         </InfoBox>
       </div>
-      <div>Welcome to the DEV React starter!</div>
     </CheckToken>
   )
 }
--- web/src/containers/Greeting/styles.js
+++ web/src/containers/Greeting/styles.js
@@ -5,6 +5,23 @@ export const InfoBox = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    position: relative;
-    margin: auto;
+    position: absolute;
+    top: 10vh;
+    height: 20vh;
+    width: 100vw;
+`
+
+export const WelcomeText = styled.p`
+    font-family: "Courier New", monospace;
+    font-size: 24px;
+    font-style: bold;
+    color: white;
+`
+
+export const CenteredText = styled.p`
+    text-align: center;
+    font-family: "Courier New", monospace;
+    font-size: 20px;
+    color: white;
+
 `
