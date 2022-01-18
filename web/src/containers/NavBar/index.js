c81990b285e0f89d25b1ec892acba47f64e18718
jhsieh731 <jhsieh@college.harvard.edu>
2022-01-18 11:23:35 -0500

    logout

--- web/src/containers/NavBar/index.js
+++ web/src/containers/NavBar/index.js
@@ -1,13 +1,26 @@
 import {FlexBox, Link} from './styles'
 import React from 'react'
+import client from '../../client'
+import { useHistory } from 'react-router-dom'
+
 
 const NavBar = () => {
+  const token = localStorage.getItem('token')
+  const history = useHistory()
+
+  const logout = () => {
+    client.resetStore()
+    localStorage.clear()
+    alert("Logged out")
+    history.push("/")
+  }
   return (
     <FlexBox>
         <Link to ="/greeting">Greeting</Link>
         <Link to="/produce">Produce</Link>
         <Link to="/procrastinate">Procrastinate</Link>
         <Link to="/social">Social</Link>
+        <button type="button" onClick={logout}>Log Out</button>
     </FlexBox>
   )
 }
