93681ace71b85cde48833678e55d3a2386ce276b
VermillionVajra <vkethamukkala@college.harvard.edu>
2022-01-18 14:02:19 -0500

    friends

---
+++ backend/src/db/migrations/20220118111404_friends.js
@@ -0,0 +1,31 @@
+const { createTableIfNotExists } = require('../helpers')
+
+exports.up = async knex => createTableIfNotExists(knex, 'friends', table => {
+  table
+    .uuid('id')
+    .notNullable()
+    .primary()
+    .defaultTo(knex.raw('uuid_generate_v4()'))
+
+  table 
+    .uuid('friend1').references('users.id')
+    .notNullable()
+
+  table
+    .uuid('friend2').references('users.id')
+    .notNullable()
+
+  table 
+    .enum('status', ['Accepted', 'Pending'])
+    .notNullable
+
+  table.timestamp('createdAt').defaultTo(knex.fn.now())
+  table.timestamp('updatedAt').defaultTo(knex.fn.now())
+})
+
+exports.down = async knex => knex.schema.dropTableIfExists('friends')
+
+// id
+//     Friend1_id(SENDER)
+//     Friend2_id(REQUESTED)
+//     Status: [accepted, pending] (ON REJECT->DELETE ROW)
