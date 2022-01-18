af362a7395c2d569eef5bb0b1d9884d267d17ead
cathystanton <cjstanton@college.harvard.edu>
2022-01-18 11:44:05 -0500

    changes

--- web/src/containers/Greeting/index.js
+++ web/src/containers/Greeting/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import CheckToken from "../CheckToken"
+import { InfoBox } from "./styles"
 
 const Greeting = () => {
   const user_id = localStorage.getItem('user_id')
   
   return (
     <CheckToken>
+      <div>
+        <InfoBox>
+          Hello, World
+        </InfoBox>
+      </div>
       <div>Welcome to the DEV React starter!</div>
     </CheckToken>
   )
---
+++ web/src/containers/Greeting/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const InfoBox = styled.div`
+    background-color: #2E294E;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    margin: auto;
+`
