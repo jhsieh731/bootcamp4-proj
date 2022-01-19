e07e0d06048fd9457bcdf218b30af809d0fa7b6e
Lydia <lshimelis11@gmail.com>
2022-01-18 20:50:06 -0700

    friend data and seed

---
+++ backend/data/friends.js
@@ -0,0 +1,19 @@
+const casual = require('casual')
+const userData = require('./users')
+
+casual.define('friend', ({user1Id, user2Id}) => ({
+    id: casual.uuid,
+    user1: user1Id,
+    user2: user2Id,
+    status: casual.random_element(['accepted', 'rejected', 'pending'])
+}))
+
+const friendData = []
+
+for (let i = 0; i < 20; i+=1) {
+    const user1Id = casual.random_element(userData).id
+    const user2Id = casual.random_element(userData).id
+    friendData.push(casual.friend({ user1Id, user2Id}))
+}
+console.log(friendData)
+module.exports = friendData
