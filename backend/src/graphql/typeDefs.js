4f16bc62428bef62c18dc8a36c35c45a33282df3
QuickPainter <painterc2003@gmail.com>
2022-01-19 10:40:02 -0500

    Added types and inputs

--- backend/src/graphql/typeDefs.js
+++ backend/src/graphql/typeDefs.js
@@ -1,14 +1,13 @@
 const { gql } = require('apollo-server-express')
 
 module.exports = gql`
-  type Mutation {
-    login(email: String!, password: String!): AuthReturn!
-    register(input: RegisterInput!): AuthReturn!
-  }
-
   type Query {
     welcome: String!
     checkToken(token: String!): Boolean!
+    user(id:ID!): User!
+    task(id:ID!): Task!
+    allTasks: [Task!]
+    allFriendships: [Friendship!] 
   }
 
   type User {
@@ -16,17 +15,54 @@ module.exports = gql`
     email: String!
     createdAt: String!
     updatedAt: String!
+    tasks: [Task!]
   }
 
   type Task {
     id: ID!
+    title: String!
     type: String! 
+    UserId: ID!
     status: String!
+  }
+
+  type Friendship {
+    id: ID!
+    Friend1_id: ID!
+    Friend2_id: ID!
+    Status: String!
   }
 
   type AuthReturn {
     token: String!
     user: User!
+  }
+  
+  type Mutation {
+    login(email: String!, password: String!): AuthReturn!
+    register(input: RegisterInput!): AuthReturn!
+    addTask(input:AddTaskInput): Task!
+    addUser(input: AddUserInput): User!
+    addFriendship(input:AddFriendshipInput): Friendship!
+  }
+
+  input AddTaskInput {
+    title: String!
+    type: String! 
+    UserId: ID!
+    status: String!
+  }
+
+  input AddUserInput {
+    email: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  input AddFriendshipInput {
+    Friend1_id: ID!
+    Friend2_id: ID!
+    Status: String!
   }
 
   input RegisterInput {
