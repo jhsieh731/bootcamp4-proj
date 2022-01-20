bcc039b25bdbcbe57f00b8a262f63d363a74e58d
cathystanton <cjstanton@college.harvard.edu>
2022-01-20 09:47:34 -0500

    updates--api still acting up

--- web/src/containers/Greeting/index.js
+++ web/src/containers/Greeting/index.js
@@ -35,26 +35,25 @@ const Greeting = () => {
     }
     return season;
   }
-  // const [temp1, setTemp1] = useState({})
-  // const [temp2, setTemp2] = useState({})
-  // const getTempDifference = async () => {
-  //     const currentUnix = Date.now()
-  //     const oldUnix = currentUnix - 1262278080000
-  //     const response1 = await fetch(`http://history.openweathermap.org/data/2.5/history/city?q=Boston,US&type=hour&start=${oldUnix}&appid=f5e62373a9abf0649138b6a25007c89e`)
-  //     const response2 = await fetch(`http://history.openweathermap.org/data/2.5/history/city?q=Boston,US&type=hour&start=${currentUnix}&appid=f5e62373a9abf0649138b6a25007c89e`)
-  //     const datum1 = await response1.json()
-  //     const datum2 = await response2.json()
-  //     console.log(datum1)
-  //     console.log(datum2)
-  //     setTemp1(datum1)
-  //     setTemp2(datum2)
-  //     return temp1.list[0].main.temp - temp2.list[0].main.temp
-  // }
-  // const direction = (getTempDifference) => {
-  //   if (getTempDifference > 0) {
-  //     return 'warmer'
-  //   } return 'colder'
-  // }
+  const [temp1, setTemp1] = useState({})
+  const [temp2, setTemp2] = useState({})
+  const getTempDifference = async () => {
+      const currentUnix = new Date().getTime()
+      const oldUnix = (currentUnix - 31556952000)
+      const response1 = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Boston,us&start=${currentUnix}&appid=2b6e1fdfed69b04f6b7eb5d94563d0ca`)
+      const response2 = await fetch(`http://history.openweathermap.org/data/2.5/history/city?q=Boston,US&type=hour&start=${oldUnix}&cnt=1&appid=2b6e1fdfed69b04f6b7eb5d94563d0ca`)
+      const datum1 = await response1.json()
+      const datum2 = await response2.json()
+      console.log(datum2)
+      setTemp1(datum1)
+      setTemp2(datum2)
+      return (temp1.main.temp - 459.67) - (temp2.list[0].main.temp - 459.67)
+  }
+  const direction = (getTempDifference) => {
+    if (getTempDifference > 0) {
+      return 'warmer'
+    } return 'colder'
+  }
   return (
     <CheckToken>
         <InfoBox>
@@ -62,7 +61,7 @@ const Greeting = () => {
                 { `Get Ready to Procrastinate with Purpose!\n
                 Today is ${ date[0] }, ${ date[1] } ${ date[2] }, ${ date[3] }.\n
                 The local time is ${ convert(date[4]) }\n
-                This ${ getSeason(new Date()) }, it is ${ /* useEffect(() => getTempDifference(), []) } \xB0F ${ direction }*/ '<waiting>' } than it was 40 years ago.\n
+                This ${ getSeason(new Date()) }, it is ${ useEffect(() => {getTempDifference()}, []) } \xB0F ${ direction } than it was 40 years ago.\n
                 In other words, the world is ending.\n
                 Put down your silly little TODO list and join our community of master procrastinators.` } />
         </InfoBox>
